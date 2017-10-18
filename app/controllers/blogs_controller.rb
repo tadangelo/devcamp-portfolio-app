711582d5dbd9b7cf876897f17075189bac9afe53
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-10-18 15:34:51 -0500

    Refactored strong params for portfolio items

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -53,7 +53,6 @@ class BlogsController < ApplicationController
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Post was successfully removed.' }
-      format.json { head :no_content }
     end
   end
 
