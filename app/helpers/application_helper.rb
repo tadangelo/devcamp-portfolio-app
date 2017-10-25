0944b4eea7cd8a55b8badd42ddabef4261999819
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-10-25 02:49:38 -0500

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,5 +19,43 @@ module ApplicationHelper
   def copyright_generator
     DangeloViewTool::Renderer.copyright "Tania D'Angelo", 'All rights reserved.' 
   end
-end
 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title:'Home'
+      },
+      {
+        url: about_me_path,
+        title:'About Me'
+      },
+      {
+        url: blogs_path,
+        title:'Blog'
+      },
+      {
+        url: portfolios_path,
+        title:'Portfolio'
+      },
+      {
+        url: contact_path,
+        title:'Contact'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>" 
+    end
+
+      nav_links.html_safe
+    end
+
+    def active? path
+      "active" if current_page? path
+    end
+end
