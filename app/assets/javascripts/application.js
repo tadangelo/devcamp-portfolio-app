8951e34ae69dc9a8bfabc90920331a22a821abe3
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-02 10:48:26 -0500

    Implemented redis library and created comment generator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,10 +14,11 @@
 //= require jquery_ujs
 //= require jquery-ui
 //= require popper
-//= require bootstrap-sprockets
 //= require rails-ujs
 //= require html.sortable
 //= require cocoon
 //= require gritter
+//= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
