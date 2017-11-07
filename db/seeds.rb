aa46ce8452ca7eb748f94e0e1b6741122ab5cdcd
Tania D'Angelo <tania.a.dangelo@outlook.com>
2017-11-06 21:09:47 -0600

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,23 @@
+User.create!(
+  email: "test@test.com",
+  password: "testtest",
+  password_confirmation: "testtest",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "test2test",
+  password_confirmation: "test2test",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
+
 3.times do |topic|
   Topic.create!(
     title: "Topic #{topic}"
